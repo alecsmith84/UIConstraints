ed4097e4dd303a72452b21ee2a867cda1370cca9
alecsmith84 <astonmartindbrs9>
2019-02-25 08:30:43 -0600

    Initial Commit

---
+++ Lifesaver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lifesaver
+//
+//  Created by Alec Smith on 2/25/19.
+//  Copyright © 2019 Alec Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
